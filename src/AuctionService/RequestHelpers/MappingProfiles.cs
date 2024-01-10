1b907e1e0ed4a15d1ea62e2fa4c6016ce39482c5
Petar Stanev <37121687+ghostdog87@users.noreply.github.com>
2024-01-10 22:03:50 +0200

    Added RabbitMQ and  consumers and producers with MassTransit.

--- src/AuctionService/RequestHelpers/MappingProfiles.cs
+++ src/AuctionService/RequestHelpers/MappingProfiles.cs
@@ -1,6 +1,7 @@
 ﻿using AuctionService.DTOs;
 using AuctionService.Entities;
 using AutoMapper;
+using Contracts;
 
 namespace AuctionService.RequestHelpers;
 
@@ -11,6 +12,9 @@ public class MappingProfiles : Profile
         CreateMap<Auction, AuctionDto>().IncludeMembers(m => m.Item);
         CreateMap<Item, AuctionDto>();
         CreateMap<CreateAuctionDto, Auction>().ForMember(d => d.Item, o => o.MapFrom(s => s));  
-        CreateMap<CreateAuctionDto, Item>();     
+        CreateMap<CreateAuctionDto, Item>();
+        CreateMap<AuctionDto, AuctionCreated>();     
+        CreateMap<Auction, AuctionUpdated>().IncludeMembers(m => m.Item);
+        CreateMap<Item, AuctionUpdated>();
     }
 }
